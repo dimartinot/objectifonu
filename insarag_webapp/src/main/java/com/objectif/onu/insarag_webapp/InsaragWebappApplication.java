9d01ee6601076730b8a59e253bbb11827d92cb12
Thomas <thomas.di.martino@eisti.fr>
2018-11-13 18:44:40 +0100

    Spring Initializr

---
+++ insarag_webapp/src/main/java/com/objectif/onu/insarag_webapp/InsaragWebappApplication.java
@@ -0,0 +1,12 @@
+package com.objectif.onu.insarag_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaragWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsaragWebappApplication.class, args);
+	}
+}
