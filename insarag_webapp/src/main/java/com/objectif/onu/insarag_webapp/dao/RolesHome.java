2bebf3fb5f1e5474039e0f0210f45b774e53edae
Thomas <thomas.di.martino@eisti.fr>
2019-01-03 17:59:31 +0100

    Generated Hibernate classes using JBoss

---
+++ insarag_webapp/src/main/java/com/objectif/onu/insarag_webapp/dao/RolesHome.java
@@ -0,0 +1,120 @@
+package com.objectif.onu.insarag_webapp.dao;
+// Generated 03-Jan-2019 17:56:06 by Hibernate Tools 5.3.0.Beta2
+
+import java.util.List;
+
+import javax.naming.InitialContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+import com.objectif.onu.insarag_webapp.model.Roles;
+
+/**
+ * Home object for domain model class Roles.
+ * @see dao.Roles
+ * @author Hibernate Tools
+ */
+public class RolesHome {
+
+	private static final Log log = LogFactory.getLog(RolesHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Roles transientInstance) {
+		log.debug("persisting Roles instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Roles instance) {
+		log.debug("attaching dirty Roles instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Roles instance) {
+		log.debug("attaching clean Roles instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Roles persistentInstance) {
+		log.debug("deleting Roles instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Roles merge(Roles detachedInstance) {
+		log.debug("merging Roles instance");
+		try {
+			Roles result = (Roles) sessionFactory.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Roles findById(int id) {
+		log.debug("getting Roles instance with id: " + id);
+		try {
+			Roles instance = (Roles) sessionFactory.getCurrentSession().get("dao.Roles", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Roles instance) {
+		log.debug("finding Roles instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("dao.Roles").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ insarag_webapp/src/main/java/com/objectif/onu/insarag_webapp/dao/UsersHome.java
@@ -0,0 +1,120 @@
+package com.objectif.onu.insarag_webapp.dao;
+// Generated 03-Jan-2019 17:56:06 by Hibernate Tools 5.3.0.Beta2
+
+import java.util.List;
+
+import javax.naming.InitialContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+import com.objectif.onu.insarag_webapp.model.Users;
+
+/**
+ * Home object for domain model class Users.
+ * @see dao.Users
+ * @author Hibernate Tools
+ */
+public class UsersHome {
+
+	private static final Log log = LogFactory.getLog(UsersHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Users transientInstance) {
+		log.debug("persisting Users instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Users instance) {
+		log.debug("attaching dirty Users instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Users instance) {
+		log.debug("attaching clean Users instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Users persistentInstance) {
+		log.debug("deleting Users instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Users merge(Users detachedInstance) {
+		log.debug("merging Users instance");
+		try {
+			Users result = (Users) sessionFactory.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Users findById(int id) {
+		log.debug("getting Users instance with id: " + id);
+		try {
+			Users instance = (Users) sessionFactory.getCurrentSession().get("dao.Users", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Users instance) {
+		log.debug("finding Users instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("dao.Users").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
